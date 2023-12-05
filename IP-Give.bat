4bed48a7a91c18639cf70f93a12ee5115d4bc0ec
FeltMacaroon389 <148586598+FeltMacaroon389@users.noreply.github.com>
2023-12-05 20:00:33 +0100

    Added better logging, and customizable file formats
    It is now optional to save the IP addresses to a log file, and if you still wish to save the IP addresses to a log file, you can chose between six different file formats:
    1. Text (.txt)
    2. Comma-Separated Values (.csv)
    3. Tab-Separated Values (.tsv)
    4. Log File (.log)
    5. Markdown (.md)
    6. JSON (.json)

--- IP-Give.bat
+++ IP-Give.bat
@@ -13,13 +13,6 @@ echo _\///////////__\///_____________________________\////////////____\///______
 echo (Made by FeltMacaroon389)
 
 echo.
-echo Look for a file named "IP_Addresses.txt" for log.
-
-echo.
-echo Scanning, please wait...
-
-echo.
-
 rem Check internet connection by pinging 8.8.8.8
 ping -n 1 8.8.8.8 | find "TTL=" > nul
 if errorlevel 1 (
@@ -28,34 +21,110 @@ if errorlevel 1 (
     goto :eof
 )
 
-if not exist IP_Addresses.txt (
-    echo. > IP_Addresses.txt
+:UserInput
+set /p saveToFile="Do you want to save IP addresses to a file? (Y/N): "
+if /i "%saveToFile%"=="N" (
+    goto :SkipFilePrompts
+) else if /i "%saveToFile%"=="Y" (
+    :Naming
+    set /p customFileName="Enter the desired file name to save IP addresses: "
+    set "fileExt="
+    if not defined customFileName (
+        echo Invalid input. File name is required.
+        goto Naming
+    )
+    goto StartFileNaming
+) else (
+    echo Invalid input. Please enter 'Y' or 'N'.
+    goto :UserInput
 )
 
-:loop
+:StartFileNaming
+echo.
+echo Choose a file format:
+echo 1. Text (.txt)
+echo 2. Comma-Separated Values (.csv)
+echo 3. Tab-Separated Values (.tsv)
+echo 4. Log File (.log)
+echo 5. Markdown (.md)
+echo 6. JSON (.json)
+set /p fileFormat="Enter the number corresponding to your choice: "
+
+if "%fileFormat%"=="1" set fileExt=".txt"
+if "%fileFormat%"=="2" (
+    set fileExt=".csv"
+    echo You chose CSV format. Make sure to use a spreadsheet program like Excel to open the file.
+)
+if "%fileFormat%"=="3" set fileExt=".tsv"
+if "%fileFormat%"=="4" set fileExt=".log"
+if "%fileFormat%"=="5" set fileExt=".md"
+if "%fileFormat%"=="6" set fileExt=".json"
+
+if not defined fileExt (
+    echo.
+    echo Invalid input. File extension is required.
+    goto StartFileNaming
+)
+
+rem Concatenate the file name and extension
+set "customFileName=!customFileName!!fileExt!"
+
+if not exist "!customFileName!" (
+    rem Create the file if it doesn't exist
+    echo Here you will see all logged IP addresses. > "!customFileName!"
+    echo. >> "!customFileName!"
+)
+echo File will be saved as: !customFileName!
+
+:SkipFilePrompts
+
+cls
+echo __/\\\\\\\\\\\__/\\\\\\\\\\\\\_____________________/\\\\\\\\\\\\____________________________________
+echo _\/////\\\///__\/\\\/////////\\\_________________/\\\//////////_____________________________________
+echo _____\/\\\_____\/\\\_______\/\\\________________/\\\______________/\\\______________________________
+echo _____\/\\\_____\/\\\\\\\\\\\\\/___/\\\\\\\\\\\_\/\\\____/\\\\\\\_\///___/\\\____/\\\_____/\\\\\\\\__
+echo _____\/\\\_____\/\\\/////////____\///////////__\/\\\___\/////\\\__/\\\_\//\\\__/\\\____/\\\/////\\\_ 
+echo _____\/\\\_____\/\\\___________________________\/\\\_______\/\\\_\/\\\__\//\\\/\\\____/\\\\\\\\\\\__ 
+echo _____\/\\\_____\/\\\___________________________\/\\\_______\/\\\_\/\\\___\//\\\\\____\//\\///////___ 
+echo __/\\\\\\\\\\\_\/\\\___________________________\//\\\\\\\\\\\\/__\/\\\____\//\\\______\//\\\\\\\\\\_ 
+echo _\///////////__\///_____________________________\////////////____\///______\///________\//////////__
+echo (Made by FeltMacaroon389)
+
+echo.
+echo Scanning, please wait...
+
+echo.
+:MainLoop
 set /a octet1=!random! %% 256
 set /a octet2=!random! %% 256
 set /a octet3=!random! %% 256
 set /a octet4=!random! %% 256
 set ip=!octet1!.!octet2!.!octet3!.!octet4!
 
-rem Check if the IP address is already logged
-findstr /x "!ip!" IP_Addresses.txt > nul
-if errorlevel 1 (
-    rem IP address not found in the log, proceed to check if it's reachable
+if /i "%saveToFile%"=="Y" (
+    rem Check if the IP address is already logged
+    findstr /x "!ip!" "!customFileName!" > nul
+    if errorlevel 1 (
+        rem IP address not found in the log, proceed to check if it's reachable
+        ping -n 1 -w 100 !ip! | find "TTL=" > nul
+        if errorlevel 1 (
+            rem IP is unreachable, do nothing
+        ) else (
+            echo !ip! is up.
+            echo !ip! is up. Date and time found: !date! !time! >> "!customFileName!"
+        )
+    ) else (
+        echo ERROR !ip! is already logged.
+    )
+) else (
+    rem If not saving to a file, simply echo the IP address
     ping -n 1 -w 100 !ip! | find "TTL=" > nul
     if errorlevel 1 (
         rem IP is unreachable, do nothing
     ) else (
         echo !ip! is up.
-        echo !ip! is up. Date found: %date% >> IP_Addresses.txt
     )
-) else (
-    echo ERROR !ip! is already logged.
 )
 
-goto loop
-
-
-
+goto MainLoop
 
